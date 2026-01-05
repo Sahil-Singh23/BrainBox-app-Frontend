12a55b9478246297455df31d63bc82ba17accd52
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-05 16:03:54 +0530

    created component files

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import PlusIcon from './icons/PlusIcon'
 import { Button } from './components/ui/Button'
 import ShareIcon from './icons/ShareIcon'
+import Card from './components/Card'
 
 
 function App() {
@@ -9,9 +10,10 @@ function App() {
 
   return (
     <>
-      {/*   */}
-      <Button variant="primary" text='Add Content' onClick={()=>{}} size="md" startIcon={<PlusIcon size="lg"></PlusIcon>}></Button>
-      <Button variant="secondary" text='Share Brain' onClick={()=>{}} size="md" startIcon={<ShareIcon size='lg'></ShareIcon>}></Button>
+      
+      {/* <Button variant="primary" text='Add Content' onClick={()=>{}} size="md" startIcon={<PlusIcon size="lg"></PlusIcon>}></Button>
+      <Button variant="secondary" text='Share Brain' onClick={()=>{}} size="md" startIcon={<ShareIcon size='lg'></ShareIcon>}></Button> */}
+      <Card></Card>
     </>
   ) 
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,25 @@
- const Card = () => {
+import ShareIcon from "../icons/ShareIcon"
+
+interface CardProps{
+    title: string,
+    link: string,
+    type: "twitter"|"youtube";
+}
+
+const Card = (props: CardProps) => {
   return (
-    <div>
+    <div className="bg-white max-w-72
+     rounded-md shadow-md p-4 border-slate-100">
+        <div className="flex justify-between">
+            <div className="flex gap-2">
+                <ShareIcon size="md"></ShareIcon>
+                Project ideas
+            </div>
+            <div className="flex gap-2">
+                 <ShareIcon size="md"></ShareIcon>
+                  <ShareIcon size="md"></ShareIcon>
+            </div> 
+        </div>
       
     </div>
   )
