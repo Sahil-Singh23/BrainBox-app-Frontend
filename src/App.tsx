88cc4131fab8d3aa6a7e4c3784425d6facb881a6
Sahil-Singh23 <sahil091205@gmail.com>
2026-01-06 09:17:03 +0530

    created card component files

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,15 @@ function App() {
   
 
   return (
-    <section className='flex gap-8 max-w-7xl justify-center mt-20'>
+    <section className='columns-1 sm:columns-2 lg:columns-3 gap-8 max-w-7xl mt-20 mx-auto'>
       
       {/* <Button variant="primary" text='Add Content' onClick={()=>{}} size="md" startIcon={<PlusIcon size="lg"></PlusIcon>}></Button>
       <Button variant="secondary" text='Share Brain' onClick={()=>{}} size="md" startIcon={<ShareIcon size='lg'></ShareIcon>}></Button> */}
       <Card type="twitter" title="first tweet"></Card>
       <Card type="youtube" title="polymath"></Card>
-      <Card type="instagram" title="damnnnn"></Card>
+      <Card type="instagram" title="valorant latest update"></Card>
+      <Card type="pinterest" title="snekars"></Card>
+
     </section>
   ) 
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,41 +5,52 @@ import { useEffect } from "react";
 interface CardProps{
     title: string,
     link: string,
-    type: "twitter"|"youtube"|"instagram";
+    type: "twitter"|"youtube"|"instagram"|"pinterest";
 }
 
 const Card = ({title,link,type}: CardProps) => {
   useEffect(() => {
+    //@ts-ignore
     if (type === "twitter" && window.twttr) {
+      //@ts-ignore
       window.twttr.widgets.load();
     }
+    //@ts-ignore
     if (type === "instagram" && window.instgrm) {
+      //@ts-ignore
       window.instgrm.Embeds.process();
+    }
+    //@ts-ignore
+    if (type === "pinterest" && window.PinUtils) {
+      //@ts-ignore
+      window.PinUtils.build();
     }
   }, [type]);
 
   return (
-    <div className="bg-white max-w-72 rounded-md shadow-md p-4 border-slate-100 flex flex-col min-h-96">
+    <div className="bg-white w-full h-auto rounded-md shadow-md p-4 border-slate-100 flex flex-col break-inside-avoid mb-8">
         <div className="flex justify-between">
             <div className="flex gap-2">
                 <NoteIcon size="lg"></NoteIcon>
-                <span>{title}</span>
+                <span className="pb-2">{title}</span>
             </div>
             <div className="flex gap-2">
                  <ShareIcon size="md"></ShareIcon>
                   <ShareIcon size="md"></ShareIcon>
             </div> 
         </div>
-        <div className="my-auto w-full max-w-full">
+        <div className="w-full">
             {(type==="twitter" && <blockquote className="twitter-tweet">
                 <a href="https://twitter.com/viralbiryani/status/2007775223089930529?s=20"></a> 
                 </blockquote>)}
 
 
-            {(type==="youtube" && <iframe className="w-full rounded-xl" src="https://www.youtube.com/embed/IkPabpDzmT4?si=pjA6rrw8c9WIKgyZ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>)}
+            {(type==="pinterest" && <a data-pin-do="embedPin" data-pin-width="medium" href="https://pin.it/5TEPsvVtW"></a>)}
+
+            {(type==="youtube" && <iframe className="w-full rounded-xl aspect-video" src="https://www.youtube.com/embed/IkPabpDzmT4?si=pjA6rrw8c9WIKgyZ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>)}
 
 
-            {(type==="instagram" && <blockquote className="instagram-media rounded-xl max-w-full" data-instgrm-permalink="https://www.instagram.com/p/DSLE8B1D6XX/?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="14" style={{background:"#FFF", border:"0", borderRadius:"3px", boxShadow:"0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)", margin:"1px", maxWidth:"calc(100% - 2px)", padding:"0"}}></blockquote>)}
+            {(type==="instagram" && <blockquote className="instagram-media" data-instgrm-permalink="https://www.instagram.com/p/DSLE8B1D6XX/?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="14" style={{background:"#FFF", border:"0", borderRadius:"8px", boxShadow:"0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)", margin:"0", width:"100%", maxWidth:"100%", minWidth:"0"}}></blockquote>)}
         </div>
     </div>
   )
